bd18c3b99cb032bb053d0aae5c8d3b8687c6944e
Daniel Vaughan <dbvaughan@gmail.com>
2017-08-04 16:25:22 +0200

    Updated for inclusion in MM Addin manager.

---
+++ Publishing/CreateDistribution.ps1
@@ -0,0 +1,35 @@
+cd "$PSScriptRoot" 
+
+$src = "$env:appdata\Markdown Monster\Addins\TocAddin"
+
+"Cleaning up build files..."
+del ..\Build\addin.zip
+
+remove-item -recurse -force ..\Build\Distribution
+md ..\Build\Distribution
+
+"Copying files..."
+copy $src\*.dll ..\Build\Distribution
+copy version.json ..\Build\Distribution
+copy version.json ..\Build\
+
+[Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem")
+
+function ZipFiles($zipfilename, $sourcedir)
+{
+   Add-Type -Assembly System.IO.Compression.FileSystem
+   $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+   [System.IO.Compression.ZipFile]::CreateFromDirectory($sourcedir,
+        $zipfilename, $compressionLevel, $false)
+}
+
+"Zipping up setup file..."
+#7z a -tzip  addin.zip ..\Distribution\*.*
+#$source = Join-Path "$($PSScriptRoot)" -ChildPath ".." | Join-Path -ChildPath "Distribution"
+# [io.path]::combine("$($PSScriptRoot)", "..\Distribution\");
+$source = (get-item "$PSScriptRoot").parent.FullName + "\Build\Distribution\*.*"
+$zipPath = (get-item "$PSScriptRoot").parent.FullName + "\Build\addin.zip"
+#ZipFiles $zipPath, $source
+Compress-Archive -Path $source -CompressionLevel Optimal -DestinationPath $zipPath
+echo $source
+echo $zipPath
---
+++ Publishing/Example.md
@@ -0,0 +1,25 @@
+# Table of Contents
+
+[//]: # (TOC Begin)
+* [This is a Title](#this-is-a-title)
+	* [This is Subsection 1](#this-is-subsection-1)
+		* [This is Subsection 2](#this-is-subsection-2)
+	* [This is Subsection 3](#this-is-subsection-3)
+
+[//]: # (TOC End)
+
+# This is a Title
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+
+## This is Subsection 1
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+
+### This is Subsection 2
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+
+## This is Subsection 3
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
